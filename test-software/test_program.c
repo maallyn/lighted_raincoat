2eaadf8c5cb0a5abc3e7b4fa90c1bfa4c92618c5
Mark Allyn <markallyn456@gmail.com>
2020-11-11 09:28:16 -0800

    Made my first test progrm and made my first Makefile

---
+++ test-software/test_program.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>
+#include <time.h>
+#include <wiringPi.h>
+
+int main(int argc, char **argv)
+{
+printf("Hello out there\n");
+}
